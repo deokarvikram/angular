cd273c0935cc0ca370b3730cec1145fd18f13e07
z004phtd <vikram.deokar@siemens.com>
2024-05-11 23:07:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { single } from 'rxjs';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { AboutComponent } from './component/about/about.component';
+import { CreateBinComponent } from './component/create-bin/create-bin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"signup", component:SignupComponent},
+    {path:"login", component:LoginComponent},
+    {path:"", redirectTo:"/login", pathMatch:"full"},
+    {path:"create", component:CreateBinComponent},
+    {path:"about", loadComponent:() => import('./component/about/about.component').then(mod => mod.AboutComponent)},
+    {path:"**", component:NotFoundComponent}
+];
